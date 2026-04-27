3414dab6f9d1c4d4a5e1cc320734023668d012f0
Ella Luize Zarane <Ella.Luize.Zarane@fita.lv>
2026-04-27 16:59:49 +0300

    feat: added a cycle counting the sum and average value of the list

--- data_collections.py
+++ data_collections.py
@@ -3,3 +3,10 @@
 numbers=[1,2,3,4,5,6,7,8,9]
 numbers.append(12)
 numbers.pop(5)
+
+tog=0
+for a in (numbers):
+    tog+=a
+    mid=tog/9
+print("Saraksta summa: ", tog)
+print("Vidējā vērtība: ", mid)
