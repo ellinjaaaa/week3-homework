df9ea0a1f8ae7b960732037529169b7a55f7d7bc
Ella Luize Zarane <Ella.Luize.Zarane@fita.lv>
2026-04-27 17:51:09 +0300

    fix: added an empty list, in order to make a new list and not impact the original one; feat: added docstrings and comments

--- data_collections.py
+++ data_collections.py
@@ -1,21 +1,27 @@
 #A daļa
 
+'''Izveidots skaitļu saraksts, kas pievieno jaunu elementu, un tad atņem noteiktu elementu (pēc indeksa).'''
 numbers=[1,2,3,4,5,6,7,8,9]
-numbers.append(12)
-numbers.pop(5)
+numbers.append(12)   #Pievienota jauna vērtība.
+numbers.pop(5)    #Atņemta specifiska vērtība (pēc indeksa).
 
-tog=0
-count=0
-for a in (numbers):
-    tog+=a
-    count+=1
+'''Cikls, kas aprēķina saraksta skaitļu summu un tad arī vidējo vērtību, neizmantojot sum() un len(), bet gan pakāpeniski
+aprēķinot.'''
+tog=0   #Sākotnējā vērtība summai, pirms sākam skaitīt.
+count=0    #Sākotnējā vērtība skaitīšanas reizei, pirms sākam skaitīt.
+for a in (numbers):  #Cikls - tik reižu, cik sarakstā elementu.
+    tog+=a    #Katru reizi summai tiek pievienots skaitlis no saraksta - sum() vietā.
+    count+=1   #Katru reizi skaitīšanas reizei tiek pievienots 1 - len() vietā.
 
-mid=tog/count
+mid=tog/count    #Vidējā vērtība.
 print("Saraksta summa: ", tog)
 print("Vidējā vērtība: ", mid)
 
-for b in (numbers):
+'''Izveidots tukšs un jauns saraksts, kuram caur ciklu ar noteiktu reižu skaitu, tiek pievienoti skaitļi no 
+numbers saraksta, ja tie ir pāra skaitļi jeb dalās ar 2 bez atlikuma.'''
+even_n=[]   #Pievienots tukšs saraksts, lai veidotos JAUNS saraksts, nevis tiktu ietekmēts oriģinālais.
+for b in (numbers):   #Cikls - tik reižu, cik sarakstā elementu.
     if b%2==0:
-        print(b)
-    else:
-        continue
+        even_n.append(b)  #Ja skaitlis ir pāra, tad tiek PIEVIENOTS tukšajam sarakstam.
+
+print("Jaunais saraksts ar pāra skaitļiem:", even_n)
