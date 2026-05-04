c567302f2761ad3d0ad23d4bd0b53e074ea676e5
Ella Luize Zarane <Ella.Luize.Zarane@fita.lv>
2026-05-04 17:43:20 +0300

    feat: added a function for validating a LV phone number

--- validators.py
+++ validators.py
@@ -21,8 +21,25 @@ def is_email(text):
         return False
     return True
 
-#def is_phone_number(text): # Latvijas formāts: +371 XXXXXXXX (8 cipari)
+def is_phone_number(text):
+    '''
+    Funkcija validē telefona numuru pēc LV nr. standartiem - +371 priekšā, tad vēl 8 cipari aiz tā.
 
+    Args:
+        text: validējamais tel. nr.
+
+    Atgriež:
+        bool: True, ja sākas ar +371, un pēc tam, neatkarīgi no atstarpēm, ievadīti 8 CIPARI. Citādāk - False.
+
+    Piemēri:
+    print(is_phone_number("+27755667788")) #False
+    print(is_phone_number("+371 22334455")) #True
+    '''
+    if not text.startswith("+371"): #.startswith() nodrošina, ka numurs sākas ar valsts kodu
+        return False
+    number=text[len("+371"):].strip() #tiek atstāts numurs bez valsts koda, kā arī tā sākuma un beigās noņemtas atstarpes
+    number=number.replace(" ","") #noņem visas atstarpes - tās, kuras numura vidū
+    return number.isdigit() and len(number) == 8 #.isdigit() nodrošina, ka tikai cipari; len() nodrošina, ka 8 cipari numurā (LV)
 
 #def is_valid_age(age): # 0–150, vesels skaitlis
 
@@ -39,3 +56,9 @@ if __name__ == "__main__":
     print(is_email("inbox.lv")) #False
     print(is_email("")) #False
     print(is_email("inbox.lv@anna")) #False
+    print(is_phone_number("+37122334455")) #True
+    print(is_phone_number("+3712233")) #False
+    print(is_phone_number("22334455")) #False
+    print(is_phone_number("")) #False
+    print(is_phone_number("+27755667788")) #False
+    print(is_phone_number("+371 22334455")) #True
